2ae60ffbecff28ee14d1dc3777184ee69baf80d0
Sage <sha.sage00@gmail.com>
2025-06-11 03:14:09 +0000

    updating script.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
 
-// TODO list of steps to run
+sudo apt update -y
+sudo apt upgrade -y
 
+sudo apt install unzip php composer nano rabbitmq-server net-tools openssh-server wget -y
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+wget https://github.com/MattToegel/IT490/archive/refs/heads/master.zip -O master.zip
+unzip master.zip
+
+
+cd IT490-master
+
+
+sed -i 's/"type": *"[^"]*"/"type": "something-fun"/' composer.json
+
+
+composer update
+composer install
+
+php RabbitMQServerSample.php
